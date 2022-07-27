1978fd50f257edb5c02e9da6243263512216abcb
Szymon Gamża <gamza.szymon@gmail.com>
2022-07-27 21:51:14 +0200

    Add project files.

---
+++ CodingTimeTracker/CodingSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTimeTracker
+{
+    public class CodingSession
+    {
+        public CodingSession(DateTime startTime, DateTime endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            Duration = CalculateTime(startTime, endTime);
+        }
+
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        private TimeSpan CalculateTime(DateTime startTime, DateTime endTime)
+        {
+            TimeSpan duration = endTime - startTime;
+            return duration;
+        }
+    }
+
+}
