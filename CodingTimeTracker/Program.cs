721c319cdc48b58f30a85090ec9201a43e68b26a
Szymon Gamża <gamza.szymon@gmail.com>
2022-07-29 15:17:29 +0200

    Added InputManager, UserInterface and ValidationInput for number and datetime

--- CodingTimeTracker/Program.cs
+++ CodingTimeTracker/Program.cs
@@ -5,15 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Some tests how TimeSpan works.
-        //DateTime startTime = DateTime.Parse("2022-10-10 10:00");
-        //DateTime endTime = DateTime.Parse("2022-10-10 12:00");
-        //CodingSession codingSession = new CodingSession(startTime,endTime);
-        //Console.WriteLine(codingSession.StartTime);
-        //Console.WriteLine(codingSession.EndTime);
-        //Console.WriteLine(codingSession.Duration);
-        //TimeSpan duration = codingSession.Duration + codingSession.Duration;
-        //Console.WriteLine(duration);
         DBManager dbManager = new DBManager();
+        UserInterface userInterface = new UserInterface();
+
     }
 }
---
+++ CodingTimeTracker/UserInterface.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTimeTracker
+{
+    public class UserInterface
+    {
+        InputManager inputManager = new InputManager();
+
+        public UserInterface()
+        {
+            MainMenuLoop();
+        }
+
+        public void MainMenuLoop()
+        {
+            Console.Clear();
+            bool quitApp = false;
+            while (quitApp == false)
+            {
+                Console.WriteLine("+---------------------------------------------+");
+                Console.WriteLine("|                   MAIN MENU                 |");
+                Console.WriteLine("+---------------------------------------------+");
+                Console.WriteLine("|    Type 0 to quit app                       |");
+                Console.WriteLine("|    Type 1 to VIEW all coding times records  |");
+                Console.WriteLine("|    Type 2 to INSERT coding time record      |");
+                Console.WriteLine("|    Type 3 to DELETE coding time record      |");
+                Console.WriteLine("|    Type 4 to UPDATE coding time record      |");
+                Console.WriteLine("+---------------------------------------------+");
+
+                var input = Console.ReadLine();
+                int option = -1;
+                while (input == null)
+                {
+                    input = Console.ReadLine();
+                }
+                try
+                {
+                    option = int.Parse(input);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                switch (option)
+                {
+                    case 0:
+                        quitApp = true;
+                        break;
+                    case 1:
+                        Console.Clear();
+                        inputManager.GetData();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        inputManager.InsertData();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        inputManager.GetData();
+                        inputManager.DeleteData();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        inputManager.GetData();
+                        inputManager.UpdateData();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("-----------------------------------------");
+                        Console.WriteLine("|       Choose option from menu!        |");
+                        Console.WriteLine("-----------------------------------------");
+                        break;
+                }
+
+            }
+        }
+    }
+}
